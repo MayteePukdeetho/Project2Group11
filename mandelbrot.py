47e2e724425b08cdcd9de44bdb7150ce20dc3237
solomiiaivanovska <ivanovska.s@ufl.edu>
2025-03-04 11:10:16 -0500

    updated the code since there was mistake in original project output

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,7 +1,10 @@
 def get_escape_time(c: complex, max_iterations: int) -> int | None:
     z = c
-    for i in range(max_iterations):
+    if abs(z) > 2:
+        return 0
+
+    for k in range(max_iterations):
+        z = z ** 2 + c
         if abs(z) > 2:
-            return i
-        z = z**2 + c
+            return k + 1
     return None
